a79b4b2efd16171e31407a249ed5f90f9c732a0d
OussamaZermouq <oussamazermouq2@gmail.com>
2024-06-15 00:24:29 +0100

    First commit

---
+++ src/main/java/com/pfa/Careersync/CareersyncApplication.java
@@ -0,0 +1,13 @@
+package com.pfa.Careersync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareersyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareersyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfa/Careersync/CareersyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfa.Careersync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareersyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
